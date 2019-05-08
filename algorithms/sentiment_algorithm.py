93250584cfd5aa3a6aa34e0255b672d507fcab30
Richard Greenbaum <rgreenbaum@Richards-MacBook-Pro.local>
2019-05-08 14:48:24 +0200

    sentiment algorithm added

---
+++ algorithms/sentiment_algorithm.py
@@ -0,0 +1,132 @@
+"""
+This is a template algorithm on Quantopian for you to adapt and fill in.
+"""
+import quantopian.algorithm as algo
+from quantopian.pipeline import Pipeline
+from quantopian.pipeline.data.builtin import USEquityPricing
+from quantopian.pipeline.filters import QTradableStocksUS
+from quantopian.pipeline.factors import SimpleMovingAverage
+from quantopian.pipeline.data.psychsignal import stocktwits
+from quantopian.pipeline.data.sentdex import sentiment
+import quantopian.optimize as opt
+
+
+def initialize(context):
+    """
+    Called once at the start of the algorithm.
+    """
+    context.max_leverage = 1.0
+    context.max_pos_size = 0.015
+    context.max_turnover = 0.95
+    
+    # Rebalance every day, 1 hour after market open.
+    algo.schedule_function(
+        rebalance,
+        algo.date_rules.every_day(),
+        algo.time_rules.market_open(hours=1),
+    )
+
+    # Record tracking variables at the end of each day.
+    algo.schedule_function(
+        record_vars,
+        algo.date_rules.every_day(),
+        algo.time_rules.market_close(),
+    )
+
+    # Create our dynamic stock selector.
+    algo.attach_pipeline(make_pipeline(), 'pipeline')
+
+
+def make_pipeline():
+    """
+    A function to create our dynamic stock selector (pipeline). Documentation
+    on pipeline can be found here:
+    https://www.quantopian.com/help#pipeline-title
+    """
+
+    # Base universe set to the QTradableStocksUS
+    base_universe = QTradableStocksUS()
+    
+    stocktwits_score = SimpleMovingAverage(
+        inputs=[stocktwits.bull_minus_bear],
+        window_length=3,
+    ).zscore()
+    
+    sentdex_score = SimpleMovingAverage(
+        inputs = [sentiment.sentiment_signal],
+        window_length = 3,
+    ).zscore()
+    
+    combined_score = (stocktwits_score + sentdex_score) / 2
+
+
+    pipe = Pipeline(
+        columns={
+            'sentiment': combined_score,
+        },
+        screen=base_universe
+    )
+    return pipe
+
+
+def before_trading_start(context, data):
+    """
+    Called every day before market open.
+    """
+    context.output = algo.pipeline_output('pipeline')
+
+    # These are the securities that we are interested in trading each day.
+    context.security_list = context.output.index
+
+
+def rebalance(context, data):
+    """
+    Execute orders according to our schedule_function() timing.
+    """
+    alpha = context.output.sentiment.dropna()
+
+    if not alpha.empty:
+        # Create MaximizeAlpha objective
+        objective = opt.MaximizeAlpha(alpha)
+
+        # Create position size constraint
+        constrain_pos_size = opt.PositionConcentration.with_equal_bounds(
+            -context.max_pos_size,
+            context.max_pos_size
+        )
+
+        # Constrain target portfolio's leverage
+        max_leverage = opt.MaxGrossExposure(context.max_leverage)
+
+        # Ensure long and short books
+        # are roughly the same size
+        dollar_neutral = opt.DollarNeutral()
+
+        # Constrain portfolio turnover
+        max_turnover = opt.MaxTurnover(context.max_turnover)
+
+        # Rebalance portfolio using objective
+        # and list of constraints
+        algo.order_optimal_portfolio(
+            objective=objective,
+            constraints=[
+                constrain_pos_size,
+                max_leverage,
+                dollar_neutral,
+                max_turnover,
+            ]
+        )
+
+
+def record_vars(context, data):
+    """
+    Plot variables at the end of each day.
+    """
+    pass
+
+
+def handle_data(context, data):
+    """
+    Called every minute.
+    """
+    pass
